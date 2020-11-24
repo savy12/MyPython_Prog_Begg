38cdc9ae508ed4b4c89d076117d6db106c242f91
savy12 <52189391+savy12@users.noreply.github.com>
2020-11-24 13:00:06 +0530

    Delete emp.py

--- emp.py
+++
@@ -1,7 +0,0 @@
-class emp:
-    def __init__(self,empno,name,c_no):
-        self.name = name
-        self.empno = empno
-        self.c_no = c_no
-        print("Employee no and name is {} {} and contact no is c_no {}".format(empno,name,c_no))
-        
