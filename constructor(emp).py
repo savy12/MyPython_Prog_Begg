d93113ccb1d0df026ce0eadc28b836fd95141029
savy12 <52189391+savy12@users.noreply.github.com>
2020-11-24 13:02:29 +0530

    Add files via upload

---
+++ constructor(emp).py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+class emp:
+    def __init__(self,empno,name,c_no):
+        self.name = name
+        self.empno = empno
+        self.c_no = c_no
+        print("Employee no and name is {} {} and contact no is c_no {}".format(empno,name,c_no))
+        
---
+++ tabels.py
@@ -0,0 +1,5 @@
+for i in range(1,20):
+    print('\n')
+    input("")
+    for j in range(1,11):
+        print(i,'*',j,'=',i*j)
